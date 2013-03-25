a40220761b77db66218a85372dd4f62a9b30c785
thomasdesvenain <thomasdesvenain@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2013-03-25 12:14:34 +0000

    Organization and contact counters links to complete list.

--- atreal/contacts/browser/directory.py
+++ atreal/contacts/browser/directory.py
@@ -1,4 +1,4 @@
-""" Define a browser view for the Contact content type. 
+""" Define a browser view for the Contact content type.
 """
 
 from Products.Five.browser import BrowserView
@@ -6,24 +6,31 @@ from Products.Five.browser import BrowserView
 class DirectoryView (BrowserView):
     """ Default view of a contact
     """
-    
+
     def search_terms(self):
         """
         """
         return self.request.form.get('search_terms', '')
-    
+
     def search_results(self):
         """
         """
         terms = self.request.form.get('search_terms', '')
+        portal_type = self.request.form.get('type', '')
         objPath = "/".join(self.context.getPhysicalPath())
         pc = self.context.portal_contacts
-        if terms == "*":
-            return pc(sort_on='sortable_title',
-                      path={'query': objPath, 'depth':1})
-        else:    
-            return pc(SearchableText=terms, sort_on='sortable_title',
-                      path={'query': objPath, 'depth':1})
+        query = dict(sort_on='sortable_title',
+                     path={'query': objPath, 'depth': 1})
+        if not terms and not portal_type:
+            return []
+
+        if terms != "*":
+            query['SearchableText'] = terms
+
+        if portal_type:
+            query['portal_type'] = portal_type
+
+        return pc(**query)
 
     def itemRowClass(self, oddity):
         """
@@ -33,11 +40,12 @@ class DirectoryView (BrowserView):
         else:
             return "even"
 
-    def commonCounter(self, type):
+    def commonCounter(self, portal_type):
         """
         """
-        return len(self.context.portal_contacts(portal_type=type,
-                      path={'query': "/".join(self.context.getPhysicalPath()), 'depth':1}))
+        return len(self.context.portal_contacts(portal_type=portal_type,
+                      path={'query': "/".join(self.context.getPhysicalPath()),
+                            'depth': 1}))
 
     def countContact(self):
         """
